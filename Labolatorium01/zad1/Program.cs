fd3a2130a9f40b651eec268c2dfb88812a2cf05a
MichalBurda4 <163707785+MichalBurda4@users.noreply.github.com>
2024-04-08 19:21:36 +0200

    Add files via upload

---
+++ Labolatorium01/zad1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Program dowolny
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+        string liczba1 = Console.ReadLine();
+
+        Console.WriteLine("Podaj drugą liczbę:");
+        string liczba2 = Console.ReadLine();
+
+        double num1, num2;
+
+        if (!double.TryParse(liczba1, out num1) || !double.TryParse(liczba2, out num2))
+        {
+            Console.WriteLine("Podane wartości nie są liczbami.");
+            return;
+        }
+
+        double sum = num1 + num2;
+        Console.WriteLine($"Wynik dodawania: {sum}");
+    }
+}
+
---
+++ Labolatorium01/zad2/Program.cs
@@ -0,0 +1,49 @@
+﻿//zad2
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "tekst.txt";
+
+        Console.WriteLine("Podaj ciąg znaków: ");
+        Console.WriteLine("Aby zakończyć, działąnie programu napisz: 'koniec!'.");
+
+        string lastString = null;
+
+        using (StreamWriter sw = new StreamWriter(filePath, append: true))
+        {
+            string input;
+            do
+            {
+                Console.Write("Wprowadź napis: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "koniec!")
+                    break;
+
+                sw.WriteLine(input);
+                lastString = input;
+
+            } while (true);
+        }
+
+        if (lastString != null)
+        {
+            Console.WriteLine($"Ostatni napis w kolejności leksykalnograficznej to : {SortString(lastString)}");
+        }
+        else
+        {
+            Console.WriteLine("Nie wprowadzono żadnych napisów.");
+        }
+    }
+
+    static string SortString(string input)
+    {
+        char[] chars = input.ToCharArray();
+        Array.Sort(chars);
+        return new string(chars);
+    }
+}
